3ba1360df912c0aff7915c3abd507abe6170688a
allenpx <allen@pixellente.com>
2014-01-30 17:47:13 -0500

    commit

---
+++ test.js
@@ -0,0 +1,6 @@
+//var config = require('./package.json').pixels;
+var pixels = require('./index');
+
+pixels.init({
+	"database":"mongodb://admin:password@novus.modulusmongo.net:27017/udewu2Gu"
+});
